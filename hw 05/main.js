10252b2781ba1e369c9ca7bff668800e06ac64a1
TheYahyo <148556053+TheYahyo@users.noreply.github.com>
2023-11-17 23:01:08 -0600

    Add files via upload

---
+++ hw 05/main.js
@@ -0,0 +1,11 @@
+
+const button = document.getElementById('button')
+const content = document.getElementById('content')
+
+
+
+button.addEventListener('click' , function () {
+    const block = document.createElement('div')
+    block.classList = 'block'
+    content.appendChild(block)
+})
